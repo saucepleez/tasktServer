043f552ac91cb7ab53ced21dd0e27110b9c9fa67
Jason Bayldon <jbayldon2005@gmail.com>
2019-02-24 01:59:06 -0500

    Added Button to create assignments

--- ClientApp/src/components/Assignments/Assignments.js
+++ ClientApp/src/components/Assignments/Assignments.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 import AssignmentList from './components/AssignmentList';
+import AddAssignment from './components/AddAssignment'
 
 export class Assignments extends Component {
     displayName = Assignments.name
@@ -10,7 +11,8 @@ export class Assignments extends Component {
         return (
 
             <div>
-                <h1>My Assignments</h1>       
+                <h1>My Assignments</h1>     
+                <AddAssignment></AddAssignment>
                 <p>Assignments are scheduled tasks that can either be executed by a single worker or a member of a specific worker pool.</p>
                 <AssignmentList api="/api/Assignments/All"></AssignmentList>
             </div>
