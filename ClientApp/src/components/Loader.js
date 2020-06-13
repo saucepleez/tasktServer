f468194f8735ff4c35ff9b0e27eb797c48bc543e
Jason Bayldon <jbayldon2005@gmail.com>
2020-06-13 00:40:00 -0400

    Update to Blazor .NET 3.1

--- ClientApp/src/components/Loader.js
+++
@@ -1,8 +0,0 @@
-﻿import React from 'react';
-import ReactLoading from 'react-loading';
- 
-const Loader = ({ type, color, width, height }) => (
-    <ReactLoading type={type} color={color} height={width} width={height} />
-);
- 
-export default Loader;
