f23158a28021555df8f936d00707764daaf08329
Jason Bayldon <jbayldon2005@gmail.com>
2020-06-14 14:45:43 -0400

    Added Workers Table

---
+++ tasktServer/Shared/Worker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tasktServer.Shared
+{
+    public class Worker
+    {
+        public Guid WorkerID { get; set; }
+        public string MachineName { get; set; }
+        public string UserName { get; set; }
+        public DateTime LastCheckIn { get; set; }
+        public WorkerStatus Status { get; set; }
+    }
+
+    public enum WorkerStatus
+    {
+        Pending = 0,
+        Authorized = 1,
+        Revoked = 2
+    }
+
+
+}
